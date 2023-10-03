ef1194670ac5a81a40597530e8a237d9f1bb9733
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-03 14:35:22 +0300

    updates the Product model

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField()
-    product_model = models.CharField()
-    launch_date = models.DateField()
+    name = models.CharField(max_length=100, verbose_name='product_name')
+    product_model = models.CharField(max_length=50, verbose_name='product_model')
+    launch_date = models.DateField(verbose_name='product_launch_date')
     is_active = models.BooleanField(default=True)

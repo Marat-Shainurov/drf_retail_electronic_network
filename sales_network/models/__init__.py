68c61d0b500c5c28af2c660abb5b8c2ef6102f6b
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-03 16:16:07 +0300

    migrates the SoleProprietor table

--- sales_network/models/__init__.py
+++ sales_network/models/__init__.py
@@ -1,5 +1,6 @@
 from .contact_info import ContactInfo
 from .factory import Factory
 from .retail_network import RetailNetwork
+from .sole_proprietor import SoleProprietor
 
-__all__ = ['Factory', 'ContactInfo', 'RetailNetwork', ]
+__all__ = ['Factory', 'ContactInfo', 'RetailNetwork', 'SoleProprietor', ]

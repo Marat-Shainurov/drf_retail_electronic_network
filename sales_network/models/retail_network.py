7cb4bb7c8d67da87f06895e2f294eb38f87fea50
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-03 16:32:21 +0300

    fixes the factory_supplier field of the RetailNetwork model

--- sales_network/models/retail_network.py
+++ sales_network/models/retail_network.py
@@ -11,7 +11,7 @@ class RetailNetwork(models.Model):
     contact_info = models.OneToOneField(ContactInfo, on_delete=models.CASCADE, related_name='network_contacts')
     products = models.ManyToManyField(Product, related_name='network_products')
     factory_supplier = models.ForeignKey(Factory, on_delete=models.SET_NULL,
-                                         verbose_name='factory_supplier', related_name='factory_supplier', **NULLABLE)
+                                         verbose_name='network_supplier', related_name='network_supplier', **NULLABLE)
     debt_to_supplier = models.DecimalField(verbose_name='debt_to_supplier', decimal_places=2, max_digits=15)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)

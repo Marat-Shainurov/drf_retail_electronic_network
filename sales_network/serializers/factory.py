d18572c52c700008539f1ad18b64926e0b7644e3
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-04 14:57:22 +0300

    adds the 'products' field to FactoryCreateSerializer

--- sales_network/serializers/factory.py
+++ sales_network/serializers/factory.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
+from products.models import Product
 from sales_network.models import Factory, ContactInfo
-from sales_network.serializers import ProductBaseSerializer, ContactInfoBaseSerializer
+from sales_network.serializers import ProductBaseSerializer, ContactInfoBaseSerializer, ProductCreateSerializer
 
 
 class FactorySerializer(serializers.ModelSerializer):
@@ -15,13 +16,19 @@ class FactorySerializer(serializers.ModelSerializer):
 
 class FactoryCreateSerializer(serializers.ModelSerializer):
     contact_info = ContactInfoBaseSerializer(many=False, required=True)
+    products = ProductCreateSerializer(many=True, required=False)
 
     class Meta:
         model = Factory
-        fields = ('name', 'contact_info',)
+        fields = ('name', 'contact_info', 'products',)
 
     def create(self, validated_data):
         contacts_data = validated_data.pop('contact_info')
+        products_data = validated_data.pop('products', [])
         contacts = ContactInfo.objects.create(**contacts_data)
         factory = Factory.objects.create(**validated_data, contact_info=contacts)
+
+        for product in products_data:
+            factory.products.create(**product)
+
         return factory

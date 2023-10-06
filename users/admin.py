6e29d2bcdd940a0977e50b5d95116cde92857605
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-06 17:16:00 +0300

    customizes and refactors the users admin interface (creation, updated and password hashing)

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,56 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 
 from users.models import CustomUser
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'is_active')
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdmin(BaseUserAdmin):
+    # form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('id', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined',)
+    list_filter = ('is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_staff', 'is_superuser', 'is_active',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
     search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, UserAdmin)
+
+admin.site.unregister(Group)

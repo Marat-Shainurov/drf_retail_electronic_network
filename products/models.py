644964d893c772cb7aad338760bb2e3167ac1bd2
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-07 12:34:13 +0300

    fixes the products and contacts models (sets the unique=True option)

--- products/models.py
+++ products/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='product_name')
+    name = models.CharField(max_length=100, verbose_name='product_name', unique=True)
     product_model = models.CharField(max_length=50, verbose_name='product_model')
     launch_date = models.DateField(verbose_name='product_launch_date')
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Config:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
--- sales_network/models/contact_info.py
+++ sales_network/models/contact_info.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class ContactInfo(models.Model):
-    email = models.EmailField(verbose_name='entity_email')
-    phone = models.CharField(max_length=20, verbose_name='entity_phone')
+    email = models.EmailField(verbose_name='entity_email', unique=True)
+    phone = models.CharField(max_length=20, verbose_name='entity_phone', unique=True)
     country = models.CharField(max_length=100, verbose_name='entity_country')
     city = models.CharField(max_length=100, verbose_name='entity_city')
     street = models.CharField(max_length=100, verbose_name='entity_street')

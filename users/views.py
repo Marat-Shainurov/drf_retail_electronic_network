95676925f38913089a0f098ce57d76b521f8f3cd
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-08 13:36:20 +0300

    adds permissions to the users ViewSet

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from users.models import CustomUser
+from users.permissions import IsUserActive
 from users.serializers import CustomUserSerializer
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
+    permission_classes = [IsUserActive, IsAdminUser]

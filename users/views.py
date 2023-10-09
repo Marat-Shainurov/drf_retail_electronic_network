65d9d4cc757c33b76d63e61267033cd428f2fff8
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-09 10:55:05 +0300

    tests the users CRUD mechanism

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
+from sales_network.pagination import BaseNetworkPagination
 from users.models import CustomUser
 from users.permissions import IsUserActive
 from users.serializers import CustomUserSerializer
@@ -10,3 +11,4 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsUserActive, IsAdminUser]
+    pagination_class = BaseNetworkPagination
